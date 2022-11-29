3c5b5851c489338a3b39c60b21d7a489f32c9115
Nicholas Fasulo <fasulo.nick@gmail.com>
2022-11-29 13:48:06 -0500

    add flip effect to cards

--- app.js
+++ app.js
@@ -72,21 +72,29 @@ easyArray = hardArray.slice(0, 16)
 // Fisher-Yates algorithm
 const shuffle = array => {
   for (let i = 0; i < array.length; i++) {
-    const j = Math.round(Math.random() * (i))
+    const j = Math.round(Math.random() * i)
     const temp = array[i]
     array[i] = array[j]
     array[j] = temp
   }
 }
 
-shuffle(hardArray)
-
 // add in if conditional based on if user clicks easy / medium / hard
-// based on what they select, that array will be passed into map function
+// based on what they select, that array will be passed into shuffle and map function
+
+shuffle(hardArray)
 
 const cardStrings = hardArray
   .map(emoji => {
-    return `<div>${emoji}</div>`
+    return `<div class="card">
+              <div class="content">
+                <div class="front">
+                </div>
+                <div class="back">
+                  ${emoji}
+                </div>
+              </div>
+            </div>`
   })
   .join('')
 
