23a68fd7fc6a43a5108bd9796974f6528ba3eb15
Nicholas Fasulo <fasulo.nick@gmail.com>
2022-11-29 12:48:03 -0500

    add shuffle function

--- app.js
+++ app.js
@@ -69,8 +69,20 @@ mediumArray = hardArray.slice(0, 32)
 
 easyArray = hardArray.slice(0, 16)
 
+// Fisher-Yates algorithm
+const shuffle = array => {
+  for (let i = 0; i < array.length; i++) {
+    const j = Math.round(Math.random() * (i))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+
+shuffle(hardArray)
+
 // add in if conditional based on if user clicks easy / medium / hard
-// based on what they select, that array will be passed into line 75
+// based on what they select, that array will be passed into map function
 
 const cardStrings = hardArray
   .map(emoji => {
@@ -81,7 +93,3 @@ const cardStrings = hardArray
 const cardNodes = document.createRange().createContextualFragment(cardStrings)
 
 document.getElementById('board').appendChild(cardNodes)
-
-// console.log(cardNodes)
-
-// console.log(cards)
